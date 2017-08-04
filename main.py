1243963d5c3cd2358bfc82af5608541bcfff64b7
fangbohui <fangbohui@163.com>
2017-08-04 02:36:59 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import random
 rng = np.random
 
 
-train_size = 100
-test_size = 10
+train_size = 1000
+test_size = 100
 dimension = 30
 
 
@@ -22,7 +22,7 @@ def make_parameters():
     # Wi * Xi
     file_parameter.write("W-dimension = %d\n" % dimension)
     for i in range(dimension):
-        file_parameter.write("%.2f\n" % random.random())
+        file_parameter.write("%.2f\n" % (random.random() / 3))
     # generate the V
     # Vij * Xi * Xj
     file_parameter.write("V-dimension = %d\n" % dimension)
@@ -31,7 +31,7 @@ def make_parameters():
             if i == j:
                 file_parameter.write("0.00\n")
             else:
-                file_parameter.write("%.2f\n" % random.random())
+                file_parameter.write("%.6f\n" % (((random.random() - 0.5)) / 100))
     file_parameter.close()
 
 
@@ -39,9 +39,9 @@ def sampling():
     # read the initial parameters and distributions
     file_parameter = open("parameter.txt", 'r', buffering = -1)
     train_X = np.zeros(train_size * dimension, dtype = float).reshape(train_size, dimension)
-    train_y = np.zeros(train_size, dtype = float)
+    train_Y = np.zeros(train_size, dtype = float)
     test_X = np.zeros(test_size * dimension, dtype = float).reshape(test_size, dimension)
-    test_y = np.zeros(test_size, dtype = float)
+    test_Y = np.zeros(test_size, dtype = float)
     prob = np.arange(dimension, dtype = float)
     W = np.zeros(dimension, dtype = float)
     V = np.zeros(dimension * dimension, dtype = float).reshape(dimension, dimension)
@@ -90,7 +90,7 @@ def sampling():
             sample = np.random.choice(field[i], 1, p = p)
             train_X[instance][cnt + sample[0]] = 1
             cnt += field[i]
-        if (instance % 10000 == 0):
+        if (instance % 100 == 0):
             print("calculated %d train_X[i]" % instance)
 
     print("begin to calc all the test_X[i]")
@@ -103,12 +103,10 @@ def sampling():
             sample = np.random.choice(field[i], 1, p = p)
             test_X[instance][cnt + sample[0]] = 1
             cnt += field[i]
-        if (instance % 10000 == 0):
+        if (instance % 100 == 0):
             print("calculated %d test_X[i]" % instance)
 
-#TODO: y[i] is too big now, you need to shrink it later
-
-    print("begin to calc all the train_y[i]")
+    print("begin to calculate all the train_Y[i]")
     for i in range(train_size):
         result = 0.0
         for j in range(dimension):
@@ -116,11 +114,11 @@ def sampling():
         for j in range(dimension):
             for k in range(dimension):
                 result += train_X[i][j] * train_X[i][k] * V[j][k]
-        train_y[i] = float(result)
-        if i % 10000 == 0:
-            print("calculated %d train_y[i]" % i)
+        train_Y[i] = float(result)
+        if i % 100 == 0:
+            print("calculated %d train_Y[i]" % i)
 
-    print("begin to calc all the test_y[i]")
+    print("begin to calculate all the test_Y[i]")
     for i in range(test_size):
         result = 0.0
         for j in range(dimension):
@@ -128,30 +126,34 @@ def sampling():
         for j in range(dimension):
             for k in range(dimension):
                 result += test_X[i][j] * test_X[i][k] * V[j][k]
-        test_y[i] = float(result)
-        # mx = max(mx, train_y[i])
-        if i % 10000 == 0:
-            print("calculated %d test_y[i]" % i)
-    return train_X, train_y, test_X, test_y
+        test_Y[i] = float(result)
+        # mx = max(mx, train_Y[i])
+        if i % 100 == 0:
+            print("calculated %d test_Y[i]" % i)
+    return train_X, train_Y, test_X, test_Y
 
-def train(tr_X, tr_y, te_X, te_y):
+
+def train(tr_X, tr_Y, te_X, te_Y):
     learning_rate = 0.01
-    training_epochs = 1000
-    display_step = 50
+    training_epochs = 10000
+    batch_size = 10
+    display_step = 1
 
     train_X = np.asarray(tr_X).reshape(train_size, dimension)
-    train_Y = np.asarray(tr_y).reshape(train_size, 1)
+    train_Y = np.asarray(tr_Y).reshape(train_size, 1)
+    test_X = np.asarray(te_X).reshape(test_size, dimension)
+    test_Y = np.asarray(te_Y).reshape(test_size, 1)
+    print(train_Y)
+    return
 
-    X = tf.placeholder("float", [None, dimension])
-    Y = tf.placeholder("float", [None, 1])
-    print("X.shape = ", X.shape)
-    print("Y.shape = ", Y.shape)
+    X = tf.placeholder("float64", [None, dimension])
+    Y = tf.placeholder("float64", [None, 1])
 
-    W_o = tf.Variable(tf.random_uniform(shape = [dimension, 1], minval = -0.1, maxval = 0.1), name = "W_o")
-    Bias_o = tf.Variable(tf.random_uniform(shape = [1], minval = -0.1, maxval = 0.1), name = "Bias_o")
+    W_o = tf.Variable(tf.random_uniform(shape = [dimension, 1], minval = -0.1, maxval = 0.1, dtype = tf.float64), name = "W_o")
+    Bias_o = tf.Variable(tf.zeros(shape = [1], dtype = tf.float64), name = "Bias_o")
 
-    pred = tf.add(tf.multiply(X, W_o), Bias_o)
-    cost = tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(pred, Y))))
+    pred = tf.add(tf.matmul(X, W_o), Bias_o)
+    cost = tf.reduce_mean(tf.sqrt(tf.square(tf.subtract(pred, Y))))
     optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
     init = tf.global_variables_initializer()
@@ -162,45 +164,29 @@ def train(tr_X, tr_y, te_X, te_y):
 
         # Fit all training data
         for epoch in range(training_epochs):
-            for (x, y) in zip(train_X, train_Y):
-                x = x.reshape(1, dimension)
-                y = y.reshape(1, 1)
-                sess.run(optimizer, feed_dict={X: x, Y: y})
+            offset = (epoch * batch_size) % (train_size - batch_size)
+            batch_X = train_X[offset:(offset + batch_size), :]
+            batch_Y = train_Y[offset:(offset + batch_size), :]
+            sess.run(optimizer, feed_dict={X: batch_X, Y: batch_Y})
 
             # Display logs per epoch step
             if (epoch + 1) % display_step == 0:
                 c = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
-                print("Epoch:", '%04d' % (epoch + 1), "cost=", "{:.9f}".format(c), \
-                      "W_o=", sess.run(W_o), "Bias_o=", sess.run(Bias_o))
+                print("Epoch:", '%04d' % (epoch + 1), "RMSE=", "{:.9f}".format(c), "Bias_o=", sess.run(Bias_o))
 
         print("Optimization Finished!")
         training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
-        print("Training cost=", training_cost, "W_o=", sess.run(W_o), "Bias_o=", sess.run(Bias_o), '\n')
-
-        # Graphic display
-        plt.plot(train_X, train_Y, 'ro', label='Original data')
-        plt.plot(train_X, sess.run(W_o) * train_X + sess.run(Bias_o), label='Fitted line')
-        plt.legend()
-        plt.show()
+        print("Training RMSE=", training_cost, "Bias_o=", sess.run(Bias_o), '\n')
 
         # Testing example, as requested (Issue #2)
-        test_X = np.asarray(te_X)
-        test_y = np.asarray(te_y)
 
         print("Testing... (Mean square loss Comparison)")
-        testing_cost = sess.run(
-            tf.reduce_sum(tf.pow(pred - Y, 2)) / (2 * test_X.shape[0]),
-            feed_dict={X: test_X, Y: test_y})  # same function as cost above
-        print("Testing cost=", testing_cost)
-        print("Absolute mean square loss difference:", abs(
-            training_cost - testing_cost))
+        testing_cost = sess.run(tf.reduce_mean(tf.sqrt(tf.square(tf.subtract(pred, Y)))), feed_dict={X: test_X, Y: test_Y})
+        print("Testing RMSE=", testing_cost)
+        print("Absolute RMSE difference:", abs(training_cost - testing_cost))
 
-        plt.plot(test_X, test_y, 'bo', label='Testing data')
-        plt.plot(train_X, sess.run(W_o) * train_X + sess.run(Bias_o), label='Fitted line')
-        plt.legend()
-        plt.show()
 
 
 make_parameters()
-train_X, train_y, test_X, test_y = sampling()
-train(tr_X = train_X, tr_y = train_y, te_X = test_X, te_y = test_y)
+train_X, train_Y, test_X, test_Y = sampling()
+train(tr_X = train_X, tr_Y = train_Y, te_X = test_X, te_Y = test_Y)
