b634a667c195df555fb65572ef8e5974fd4a0710
fangbohui <fangbohui@sjtu.edu.cn>
2017-08-16 14:21:04 +0800

    This version is good for now.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from __future__ import print_function
+from sklearn.metrics import roc_auc_score
 import tensorflow as tf
 import numpy as np
 import pylab as auc_curve
 import random
 import time
 import os
-from sklearn.metrics import roc_auc_score
 
 tag = '' + time.strftime('%m-%d-%H:%M:%S', time.localtime(time.time()))
 logdir = 'log/' + tag
@@ -14,13 +14,15 @@ print("dir = ", logdir)
 
 train_size = 1000
 test_size = 300
-field_number = 20
-y_scale = 0
-field = np.random.random_integers(1, 20, field_number)
+field_number = 35
+y_scale = 10000
+field = np.random.random_integers(1, 40, field_number)
 dimension = field.sum()
-network = np.array([dimension, 64, 1])
+network = np.array([dimension, 128, 128, 1])
 
-from scipy.stats import ttest_ind
+learning_rate = 0.1
+training_epochs = 1000
+display_step = 10
 
 print("field:")
 print(field)
@@ -48,19 +50,15 @@ def make_parameters():
         file_parameter.write("%.6f\n" % (random.random()))
     file_parameter.write("W-dimension = %d\n" % dimension)
     for i in range(dimension):
-        # file_parameter.write("%.6f\n" % (random.random() / 10))
         file_parameter.write("0.0\n")
     file_parameter.write("V-dimension = %d\n" % dimension)
     V = np.zeros([dimension, dimension])
     for i in range(dimension):
         for j in range(dimension):
-            #if (np.random.random() < 0.):
             V[i][j] = (random.random() - 0.5) * 1000
             file_parameter.write("%.6f\n" % V[i][j])
-            # else:
-            #     file_parameter.write("0.0\n")
     file_parameter.close()
-    print (np.std(V))
+    print ("the stddev of V-distribution is %.5f" % np.std(V))
 
 
 prob = np.arange(dimension, dtype = float)
@@ -104,6 +102,7 @@ def get_parameters():
     p = np.expand_dims(x, 2)
     q = np.expand_dims(x, 1)
     y = x.dot(W) + (p * q).reshape([-1, dimension**2]).dot(V.flatten())
+    print ("y-std = %.5f" % np.std(y))
     for i in range(train_size):
         y[i] = np.random.normal(loc=y[i], scale=y_scale, size=None)
     sum = 0.0
@@ -115,14 +114,17 @@ def get_parameters():
 
 def sampling(size):
     x = np.zeros([size, dimension])
-    x_indices = np.zeros([size, field_number], dtype = int)
+    x_indices = np.zeros([size * field_number, 2], dtype=int)
     cnt = 0
+    tmp = 0
     for i in range(field_number):
         p = prob[cnt:cnt+field[i]]
         sample = np.random.choice(field[i], size, p = p)
         for j in range(size):
             x[j][cnt + sample[j]] = 1
-            x_indices[j][i] = cnt + sample[j]
+            x_indices[tmp][0] = j
+            x_indices[tmp][1] = cnt + sample[j]
+            tmp += 1
         cnt += field[i]
 
     p = np.expand_dims(x, 2)
@@ -142,10 +144,9 @@ def sampling(size):
     #     print(p_value)
     # print(np.nanmean(p_values))
     # exit(0)
-    # return train_X, train_Y, test_X, test_Y
-    # y = np.asarray(y).reshape([size, 1])
-    # x_indices.sort()
-    return x_indices, y
+    y = np.asarray(y).reshape(size, 1)
+    xx = tf.SparseTensorValue(x_indices, np.ones([size * field_number]), [size, dimension])
+    return xx, y
 
 
 def get_variable(init_type='tnormal', shape=None, name=None, minval=-0.1, maxval=0.1, mean=None, stddev=None, dtype=tf.float64):
@@ -166,29 +167,12 @@ def get_variable(init_type='tnormal', shape=None, name=None, minval=-0.1, maxval
 
 
 def train():
-    learning_rate = 0.1
-    training_epochs = 600
-    display_step = 10
     config_parameter.write("learning_rate = %.5f\n" % learning_rate)
     config_parameter.write("training epochs = %d\n" % training_epochs)
     config_parameter.write("display_step = %d\n" % display_step)
-    config_parameter.write("layer_number = 2\n")
+    config_parameter.write("hidden_layer_number = %d\n" % (network.size - 2))
 
-    # train_X = np.asarray(tr_X).reshape(train_size, dimension)
-    tr_X = tf.placeholder("float64", [train_size, 2])
-    tr_Y = tf.placeholder("float64", [train_size, 1])
-    te_X = tf.placeholder("float64", [test_size, 2])
-    te_Y = tf.placeholder("float64", [test_size, 1])
-    train_X = tf.SparseTensorValue(tr_X, np.ones([train_size * field_number]), [train_size, dimension])
-    test_X = tf.SparseTensorValue(te_X, np.ones([test_size * field_number]), [test_size, dimension])
-    # train_Y = np.asarray(tr_Y).reshape()
-    # test_X = np.asarray(te_X).reshape(test_size, dimension)
-    # test_Y = np.asarray(te_Y)
-
-    # X = tf.placeholder("float64", [None, dimension])
-    # Y = tf.placeholder("float64", [None, 1])
     X = tf.sparse_placeholder("float64", [None, dimension])
-    # X = tf.placeholder("int32", [None, field_number])
     Y = tf.placeholder("float64", [None, 1])
 
     W_h = []
@@ -199,7 +183,6 @@ def train():
         Bias_h.append(get_variable('zero', [network[i]], "Bias_h" + str(i)))
     for i in range(network.size - 1):
         if (i == 0):
-            # W_h[0] = tf.gather(W_h[0], train_x_indices)
             l.append(tf.nn.tanh(tf.add(tf.sparse_tensor_dense_matmul(X, W_h[0]), Bias_h[0])))
         else:
             l.append(tf.nn.tanh(tf.add(tf.matmul(l[i - 1], W_h[i]), Bias_h[i])))
@@ -215,47 +198,51 @@ def train():
     epoch_vector = np.zeros([training_epochs / display_step + 1])
     AUC_vector = np.zeros([training_epochs / display_step + 1])
 
-    # Launch the graph
     with tf.Session() as sess:
         sess.run(init)
         sess.run(tf.initialize_local_variables())
-        # Fit all training data
         for epoch in range(training_epochs):
-            x_indices, y = sampling(train_size)
-            y = np.asarray(y).reshape(train_size, 1)
-            print (x_indices)
-            x = sess.run(train_X, feed_dict={tr_X:x_indices})
-            return
-            sess.run(optimizer, feed_dict={X: train_X, tr_Y: y})
-            # Display logs per epoch step
+            x, y = sampling(train_size)
+            sess.run(optimizer, feed_dict={X: x, Y: y})
             if (epoch + 1) % display_step == 0:
-                prediction = sess.run(pred, feed_dict={tr_X:x_indices})
+                prediction = sess.run(pred, feed_dict={X:x})
                 train_auc = roc_auc_score(y.flatten(), prediction.flatten())
-                training_cost = sess.run(cross_entropy, feed_dict={tr_X: x_indices, tr_Y: y})
+                training_cost = sess.run(cross_entropy, feed_dict={X: x, Y: y})
                 print("Epoch:", '%04d' % (epoch + 1), "Training loss =", "%.9f" % training_cost.sum(), "AUC=", train_auc, "Bias_o=", sess.run(Bias_h[network.size - 2]))
-                x_indices, y = sampling(test_size)
-                testing_cost = sess.run(cross_entropy, feed_dict={te_X: x_indices, te_Y: y})
-                prediction = sess.run(pred, feed_dict={te_X: x_indices})
+                x, y = sampling(test_size)
+                testing_cost = sess.run(cross_entropy, feed_dict={X: x, Y: y})
+                prediction = sess.run(pred, feed_dict={X: x})
                 test_auc = roc_auc_score(y.flatten(), prediction.flatten())
                 print("Testing loss = ", "%.9f" % (testing_cost.sum()), "AUC=", test_auc)
                 epoch_vector[(epoch + 1) / display_step] = epoch
                 AUC_vector[(epoch + 1) / display_step] = test_auc
 
         print("\nOptimization Finished!")
-        prediction = sess.run(pred, feed_dict={X:train_X})
-        train_auc = roc_auc_score(train_Y.flatten(), prediction.flatten())
-        training_cost = sess.run(cross_entropy, feed_dict={X: train_X, Y: train_Y})
+        x, y = sampling(train_size)
+        prediction = sess.run(pred, feed_dict={X:x})
+        train_auc = roc_auc_score(y.flatten(), prediction.flatten())
+        training_cost = sess.run(cross_entropy, feed_dict={X: x, Y: y})
         print("Training loss =", "%.9f" % training_cost.sum(), "AUC=", (train_auc), "Bias_o=", sess.run(Bias_h[network.size - 2]))
-        # Testing example, as requested (Issue #2)
 
         print("Testing... (Cross Entropy)")
-        testing_cost = sess.run(cross_entropy, feed_dict={X: test_X, Y: test_Y})
-        prediction = sess.run(pred, feed_dict={X:test_X})
-        train_auc = roc_auc_score(test_Y.flatten(), prediction.flatten())
+        x, y = sampling(test_size)
+        testing_cost = sess.run(cross_entropy, feed_dict={X: x, Y: y})
+        prediction = sess.run(pred, feed_dict={X:x})
+        test_auc = roc_auc_score(y.flatten(), prediction.flatten())
         print("Testing loss = ", "%.9f" % (testing_cost.sum()), "AUC=", (test_auc))
         print("Absolute Loss Difference:", abs(training_cost.sum() - testing_cost.sum()))
         config_parameter.write("Testing loss = %.9f\n" % (testing_cost.sum()))
         config_parameter.write("AUC = %.9f\n" % (test_auc))
+        avg_auc = 0
+        cnt = 0
+        reversed_auc = AUC_vector[::-1]
+        for i in range(reversed_auc.size):
+            avg_auc += reversed_auc[i]
+            cnt +=1
+            if (cnt == 100):
+                break
+        avg_auc /= cnt
+        config_parameter.write("AUC-AVG = %.9f\n" % avg_auc)
         config_parameter.close()
 
     print (epoch_vector)
@@ -269,5 +256,4 @@ def train():
 
 make_parameters()
 get_parameters()
-# train_X, train_Y, test_X, test_Y = sampling()
 train()
